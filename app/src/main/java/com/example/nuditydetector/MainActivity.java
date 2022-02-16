8dd897e682b09bd8844a2803008e3defbbf17450
smilingprogrammer <abdulsoburoyewale@gmail.com>
2022-02-16 22:04:44 +0100

    Updating

--- app/src/main/java/com/example/nuditydetector/MainActivity.java
+++ app/src/main/java/com/example/nuditydetector/MainActivity.java
@@ -7,9 +7,16 @@ import android.view.View;
 
 import com.example.nuditydetector.databinding.ActivityMainBinding;
 
+import java.io.File;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    String filePath = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +25,16 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        uploadImage();
+    }
+
+    private void uploadImage() {
+
+        File file = new File(filePath);
+        RequestBody requestBody = RequestBody.create(MediaType.parse(""), file);
+        MultipartBody.Part part = MultipartBody.Part.createFormData("newImage", file.getName(), requestBody);
+
+        RequestBody someData = RequestBody.create(MediaType.parse(""), "");
     }
 }
