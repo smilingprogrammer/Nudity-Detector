a7d16470e81228b747064446346cbeb9c629aaa0
smilingprogrammer <abdulsoburoyewale@gmail.com>
2022-04-07 09:59:01 +0100

    update

--- app/src/main/java/com/example/nuditydetector/MainActivity.java
+++ app/src/main/java/com/example/nuditydetector/MainActivity.java
@@ -1,38 +1,98 @@
 package com.example.nuditydetector;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-
 import android.Manifest;
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.nuditydetector.databinding.ActivityMainBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.label.ImageLabel;
+import com.google.mlkit.vision.label.ImageLabeler;
+import com.google.mlkit.vision.label.ImageLabeling;
+import com.google.mlkit.vision.label.defaults.ImageLabelerOptions;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
+    ImageView aiPicture;
+    TextView textView;
+    Button button;
+
+    private static final String TAG = "Debug";
 
     private static final int CAMERA_PERMISSION_CODE = 223;
     private static final int READ_STORAGE_PERMISSION_CODE = 144;
     private static final int WRITE_STORAGE_PERMISSION_CODE = 144;
 
+    ActivityResultLauncher<Intent> cameraLauncher;
+    ActivityResultLauncher<Intent> galleryLauncher;
+    InputImage inputImage;
+    ImageLabeler imageLabeler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        aiPicture = findViewById(R.id.aiPicture);
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        View view = binding.getRoot();
-        setContentView(view);
+        imageLabeler = ImageLabeling.getClient(ImageLabelerOptions.DEFAULT_OPTIONS);
 
-        binding.button.setOnClickListener(new View.OnClickListener() {
+        cameraLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        Intent data = result.getData();
+                        try {
+                            Bitmap photo = (Bitmap) data.getExtras().get("data");
+                            aiPicture.setImageBitmap(photo);
+                            inputImage = InputImage.fromBitmap(photo, 0);
+                            processImage();
+                        } catch (Exception e){
+                            Log.d(TAG, "OnActivityResult is: " + e.getMessage());
+                        }
+                    }
+                });
+
+        galleryLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        Intent data = result.getData();
+                        try {
+                            inputImage = InputImage.fromFilePath(MainActivity.this, data.getData());
+                            aiPicture.setImageURI(data.getData());
+                            processImage();
+                        } catch (Exception e){
+                            Log.d(TAG, "onActivityResult" + e.getMessage());
+                        }
+                    }
+                });
+
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -43,13 +103,42 @@ public class MainActivity extends AppCompatActivity {
                 builder.setItems(options, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-
+                        if (which == 0){
+                            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                            cameraLauncher.launch(cameraIntent);
+                        } else {
+                            Intent storageIntent = new Intent();
+                            storageIntent.setType("image/*");
+                            storageIntent.setAction(Intent.ACTION_GET_CONTENT);
+                            galleryLauncher.launch(storageIntent);
+                        }
                     }
                 });
             }
         });
     }
 
+    private void processImage() {
+        imageLabeler.process(inputImage)
+                .addOnSuccessListener(new OnSuccessListener<List<ImageLabel>>() {
+                    @Override
+                    public void onSuccess(List<ImageLabel> imageLabels) {
+                        String results = "";
+                        for (ImageLabel imageLabel: imageLabels){
+                            results = results + "\n" + imageLabel.getText();
+                        }
+                        textView.setText(results);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, "OnFailure: " + e.getMessage());
+                    }
+                });
+    }
+
+    //Request Permission
     @Override
     protected void onResume() {
         super.onResume();
